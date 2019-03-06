e3f1dcaf7d9ec1abfcfcdc8fbe4901ef558b3e7d
H. Watanabe <kaityo@users.sourceforge.jp>
2019-03-06 13:20:35 +0900

    adds results on hsw

--- test.cpp
+++ test.cpp
@@ -6,30 +6,34 @@ const int N = 100000;
 const int M = 1000;
 __attribute__((aligned(64))) double z[N] = {};
 
-void func(){
-  for(int i=0;i<N;i++){
+void func() {
+  for (int i = 0; i < N; i++) {
     z[i] = z[i] * z[i] + z[i];
   }
 }
 
-void func_avx2(){
-  for(int i=0;i<N;i+=4){
-    __m256d zi = _mm256_load_pd(z+i);
+#ifdef AVX2
+void func_avx2() {
+  for (int i = 0; i < N; i += 4) {
+    __m256d zi = _mm256_load_pd(z + i);
     zi = zi * zi + zi;
-    _mm256_store_pd(z+i, zi);
+    _mm256_store_pd(z + i, zi);
   }
 }
+#endif
 
-void func_avx512(){
-  for(int i=0;i<N;i+=8){
-    __m512d zi = _mm512_load_pd(z+i);
+#ifdef AVX512
+void func_avx512() {
+  for (int i = 0; i < N; i += 8) {
+    __m512d zi = _mm512_load_pd(z + i);
     zi = zi * zi + zi;
-    _mm512_store_pd(z+i, zi);
+    _mm512_store_pd(z + i, zi);
   }
 }
+#endif
 
-int main(){
-  for(int i=0;i<M;i++){
+int main() {
+  for (int i = 0; i < M; i++) {
 #ifdef AVX2
     func_avx2();
 #elif AVX512
